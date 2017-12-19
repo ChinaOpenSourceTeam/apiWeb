1b1f2004e6bd71984e8442b25dfcc215ea1aa7fe
weijiqiang <weijq@cychina.cn>
2017-12-19 20:19:08 +0800

    carousel

--- src/react/components/mainPage/carousel.js
+++ src/react/components/mainPage/carousel.js
@@ -13,14 +13,14 @@ export class CarouselShow extends React.Component {
 
     render() {
 
-        return (<div style={{width:946,height:272}}>
+        return (<div style={{width:946,height:'100%'}}>
             <Carousel autoplay>
                 <div><h3>1</h3></div>
                 <div><h3>2</h3></div>
                 <div><h3>3</h3></div>
                 <div><h3>4</h3></div>
             </Carousel>
-            <div><h2>上方固定下方自适应</h2></div>
+            <div><h2>走马灯测试中...</h2></div>
         </div>)
     }
 }
