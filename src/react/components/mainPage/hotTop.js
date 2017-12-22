3c393abfc4ef96388e9630d8816dfce43d967097
weijiqiang <weijq@cychina.cn>
2017-12-22 16:26:08 +0800

    style modify

--- src/react/components/mainPage/hotTop.js
+++ src/react/components/mainPage/hotTop.js
@@ -16,11 +16,11 @@ export class HotTop extends React.Component {
         return (
             <div className={styles.hotTop}>
                 <ul>
-                    <li style={{background:'#A98DF4'}}><a href="">最新文章 ></a></li>
-                    <li style={{background:'#3EBEF0'}}><a href="">一周最热 ></a></li>
-                    <li style={{background:'#F5E000'}}><a href="">一月最热 ></a></li>
-                    <li style={{background:'#EB545B'}}><a href="">季度最热 ></a></li>
-                    <li style={{background:'#FF9DA5'}}><a href="">年度最热 ></a></li>
+                    <li style={{background:'rgba(169, 141, 244,.7)'}}><a href="">最新文章 ></a></li>
+                    <li style={{background:'rgba(62, 190, 240,.7)'}}><a href="">一周最热 ></a></li>
+                    <li style={{background:'rgb(198, 227, 118)'}}><a href="">一月最热 ></a></li>
+                    <li style={{background:'rgba(235, 84, 91,.7)'}}><a href="">季度最热 ></a></li>
+                    <li style={{background:'rgba(255, 157, 165,.7)'}}><a href="">年度最热 ></a></li>
                 </ul>
             </div>
         )
