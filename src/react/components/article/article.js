15047607f274ec668c53f93f11d43e1204d9752a
weijq <772981712@qq.com>
2018-01-28 23:54:07 +0800

    article show content

--- src/react/components/article/article.js
+++ src/react/components/article/article.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Carousel, Row, Col, Menu, Icon, Avatar} from 'antd';
-
+import { Carousel, Row, Col, Menu, Icon, Avatar, message } from 'antd';
+import axios from "axios";
+import { config } from "../../../utils/config";
+import { Base64 } from 'js-base64';
 import styles from './article.css';
 
 
@@ -11,28 +13,76 @@ export default class Artice extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false
+            loading: false,
+            initStatus: false,
+            articleInfo: {},
         };
     }
 
+    componentDidMount() {
+        let _self = this;
+        let url = "/blog/56ec22c1-5030-4e3b-8509-066a440f8331?version=1"
+        axios.get(url, config)
+            .then(function (res) {
+                if (res.data.code == 0) {
+                    let content = _self.contentHtmlFormat(res.data.data.blog.content);
+                    console.log(content);
+                    _self.setState({ articleInfo: res.data.data, content:content,initStatus: true });
+                } else {
+                    message.error('请求失败！');
+                }
+            })
+            .catch(function (err) {
+                message.error('请求失败！' + err);
+            })
+
+    }
+
+    contentHtmlFormat = (articleContent) => {
+        let content = Base64.decode(articleContent);//content是后台返回的未知的一长串字符串，可能是'<p>内容<div>一个div</div></p>',也可能是'内容\r\n任何格式'
+        let reg = new RegExp('^<([^>\s]+)[^>]*>(.*?<\/\\1>)?$');
+        let format = reg.test(content); //content有可能是有格式的（带html标签），也可能无格式
+        if (!format) {
+            content = content && content.split('\n').map((item, i) => <p key={i}>{item.replace(/(^\s*)|(\s*$)/g, "")}</p>);
+        } else {
+            content = content && content.replace(/\n/g, "<br />");
+            //带格式的可能含有换行的/n，要转化为<br />
+        }
+
+        let contentHtml1 = <article id='contentHtml' className='content' dangerouslySetInnerHTML={{ __html: content }}></article>;
+        let contentHtml2 = <article id='contentHtml' className='content no-fomat'>{content}</article>;
+        let contentHtml = format ? contentHtml1 : contentHtml2;
+        return contentHtml;
+    }
+
     render() {
-
-
+        debugger;
         return (
             <div className={styles.articleContent}>
-                <div>
-                    <h1 className={styles.title}>花开后花又落 轮回也没结果</h1>
-                    
-                    <a className={styles.userImg} id="userImg">
-                        <Avatar size='size' style={{ color: '#f56a00', backgroundColor: '#fde3cf', fontSize: 12 }}>U</Avatar>
-                        <span className={styles.name}>尤为</span>
-                        <span className={styles.time}>2小时前</span>
-                        {/* <span>{new Date()}</span> */}
-                    </a>
-                </div>
-                <article></article>
-                <footer></footer>
+
+                
+                    <div>
+                        <h1 className={styles.title}>{this.state.initStatus ? this.state.articleInfo.blog.title : null}</h1>
+                        <div className={styles.authorContent}>
+                            <ul>
+                                <li>
+                                    <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf', fontSize: 16 }}>{this.state.initStatus ? this.state.articleInfo.user.loginName.substr(0, 1) : null}</Avatar></a>
+                                    <a className={styles.follow}>
+                                        <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                                    <a className={styles.name}>{this.state.initStatus ? this.state.articleInfo.user.loginName : null}</a>
+                                    <p>写了2222.1k字 · 25.5k喜欢</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <article>
+                        {this.state.initStatus ? this.state.content : null}
+                    </article>
+                    <footer></footer>
+                
+
             </div>
+
         )
     }
 }
--- src/react/components/mainPage/authorRe.js
+++ src/react/components/mainPage/authorRe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel ,Row,Avatar} from 'antd';
+import { Carousel, Row, Avatar } from 'antd';
 import styles from './authorRe.css';
 
 export class AuthorRe extends React.Component {
@@ -15,49 +15,49 @@ export class AuthorRe extends React.Component {
 
         return (
             <div className={styles.authorContent}>
-            <div className={styles.title}>
-                <span>作者推荐</span>
-                <a className={styles.authorRefresh}><i className="fa fa-refresh" aria-hidden="true"></i> 换一批</a>
+                <div className={styles.title}>
+                    <span>作者推荐</span>
+                    <a className={styles.authorRefresh}><i className="fa fa-refresh" aria-hidden="true"></i> 换一批</a>
+                </div>
+                <ul>
+                    <li>
+                        <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf', fontSize: 16 }}>U</Avatar></a>
+                        <a className={styles.follow}>
+                            <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                        <a className={styles.name}>尤为</a>
+                        <p>写了2222.1k字 · 25.5k喜欢</p>
+                    </li>
+                    <li>
+                        <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#5A9EDC', backgroundColor: '#A8E7F5', fontSize: 16 }}>S</Avatar></a>
+                        <a className={styles.follow}>
+                            <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                        <a className={styles.name}>沈炼</a>
+                        <p>写了418.6k字 · 22.5k喜欢</p>
+                    </li>
+                    <li>
+                        <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#EEB902', backgroundColor: '#FFD978', fontSize: 16 }}>Z</Avatar></a>
+                        <a className={styles.follow}>
+                            <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                        <a className={styles.name}>张琳</a>
+                        <p>写了418.6k字 · 22.5k喜欢</p>
+                    </li>
+                    <li>
+                        <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#8ABA04', backgroundColor: '#C6E376', fontSize: 16 }}>L</Avatar></a>
+                        <a className={styles.follow}>
+                            <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                        <a className={styles.name}>李丹</a>
+                        <p>写了418.6k字 · 22.5k喜欢</p>
+                    </li>
+                    <li>
+                        <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#4251A8', backgroundColor: '#A9AFD7', fontSize: 16 }}>C</Avatar></a>
+                        <a className={styles.follow}>
+                            <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
+                        <a className={styles.name}>菜全</a>
+                        <p>写了418.6k字 · 22.5k喜欢</p>
+                    </li>
+                </ul>
+                <a className={styles.authorMore}>更多...</a>
             </div>
-                 <ul>
-                     <li>
-                         <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf',fontSize:16}}>U</Avatar></a>
-                         <a className={styles.follow}>
-                         <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
-                         <a className={styles.name}>尤为</a>
-                         <p>写了2222.1k字 · 25.5k喜欢</p>
-                     </li>
-                     <li>
-                         <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#5A9EDC', backgroundColor: '#A8E7F5',fontSize:16}}>S</Avatar></a>
-                         <a className={styles.follow}>
-                         <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
-                         <a className={styles.name}>沈炼</a>
-                         <p>写了418.6k字 · 22.5k喜欢</p>
-                     </li>
-                     <li>
-                         <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#EEB902', backgroundColor: '#FFD978',fontSize:16}}>Z</Avatar></a>
-                         <a className={styles.follow}>
-                         <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
-                         <a className={styles.name}>张琳</a>
-                         <p>写了418.6k字 · 22.5k喜欢</p>
-                     </li>
-                     <li>
-                         <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#8ABA04', backgroundColor: '#C6E376',fontSize:16}}>L</Avatar></a>
-                         <a className={styles.follow}>
-                         <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
-                         <a className={styles.name}>李丹</a>
-                         <p>写了418.6k字 · 22.5k喜欢</p>
-                     </li>
-                     <li>
-                         <a className={styles.userImg} id="userImg"><Avatar size='large' style={{ color: '#4251A8', backgroundColor: '#A9AFD7',fontSize:16}}>C</Avatar></a>
-                         <a className={styles.follow}>
-                         <i className="fa fa-plus" aria-hidden="true"></i> 关注</a>
-                         <a className={styles.name}>菜全</a>
-                         <p>写了418.6k字 · 22.5k喜欢</p>
-                     </li>
-                 </ul>
-                 <a className={styles.authorMore}>更多...</a>
-                  </div>
         )
     }
 }
